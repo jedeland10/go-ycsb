8fee9a6e38dad40bec1f765ce61bbd8afb0a547c
jedeland10 <johan.edeland@live.com>
2025-04-03 10:27:19 +0200

    fixed run scripts

--- run_cloud_bench.sh
+++ run_cloud_bench.sh
@@ -1,6 +1,6 @@
 # Ensure we have exactly four parameters
 if [ "$#" -ne 7 ]; then
-  echo "Usage: $0 <output_file_path> <run_index> <keysize> <record_count> <operation_count> <thread_count> <endpoint>"
+  echo "Usage: $0 <output_file_path> <run_index> <keysize> <endpoint> <record_count> <operation_count> <thread_count>"
   exit 1
 fi
 
@@ -8,10 +8,10 @@ fi
 output_file_path="$1"   # e.g. "../ycsb_bench/cache/once"
 run_index="$2"          # e.g. "1"
 keysize="$3"      # e.g. "4096"
-record_count="$4"
-operation_count="$5"
-thread_count="$6"
-endpoint="$7"
+endpoint="$4"
+record_count="$5"
+operation_count="$6"
+thread_count="$7"
 
 # Build the output file name: output_file_path/{keyprefixsize}_{run_index}.txt
 output_file="${output_file_path}/${keysize}_${run_index}.txt"
@@ -25,6 +25,6 @@ output_file="${output_file_path}/${keysize}_${run_index}.txt"
     -p keypsize="$keysize" \
     -p threadcount="$thread_count" \
     -p recordcount="$record_count" -p operationcount="$operation_count" 2>&1 \
-    | grep -E '^(INSERT|TOTAL)' | tee -a "$output_file"
+    | grep -E '^(UPDATE|TOTAL)' | tee -a "$output_file"
 
 echo "---------------------------------------" | tee -a "$output_file"
